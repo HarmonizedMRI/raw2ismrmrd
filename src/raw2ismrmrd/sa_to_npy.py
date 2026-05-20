5bfe3d19e1fefc4e74a11fb9876569d6b9c32b41
Christoph Kolbitsch <christoph.kolbitsch@ptb.de>
2026-05-20 11:13:36 +0100

    fix mypy errors

--- src/raw2ismrmrd/sa_to_npy.py
+++ src/raw2ismrmrd/sa_to_npy.py
@@ -4,6 +4,7 @@ Convert raw data from a scan archive file to a numpy array.
 
 - Assumes resolution of each readout is the same
 """
+
 import argparse
 import logging
 from pathlib import Path
@@ -13,39 +14,47 @@ from GERecon import Archive
 
 logging.basicConfig(level=logging.INFO)
 
+
 def get_useful_packets(archive_filename: str) -> int:
     """
     Read a ScanArchive file and count useful opcodes.
 
-    Args:
-        archive_filename (str): Path to an existing ScanArchive*.h5 file.
+    Parameters
+    ----------
+    archive_filename
+        Path to an existing ScanArchive*.h5 file.
 
-    Returns:
+    Returns
+    -------
         sum where opcode = 1
 
     """
     archive = Archive(archive_filename)
     metadata = archive.Metadata()
-    num_control = metadata["controlCount"]
+    num_control = metadata['controlCount']
     opcode = []
 
     # Loop over all the control packets
-    for i in range(num_control):
+    for _ in range(num_control):
         # Retrieve the next control packet
         control = archive.NextControl()
-        opcode.append(control["opcode"])
+        opcode.append(control['opcode'])
 
     return opcode.count(1)
 
-def read_archive(archive_filename: str) -> np.complex64:
+
+def read_archive(archive_filename: str) -> np.ndarray:
     """
     Read a ScanArchive file and convert to a numpy array.
 
-    Args:
-        archive_filename (str): Path to an existing ScanArchive*.h5 file.
+    Parameters
+    ----------
+    archive_filename
+        Path to an existing ScanArchive*.h5 file.
 
-    Returns:
-        data_out: numpy array
+    Returns
+    -------
+        data_out
 
     """
     # First figure out how many useful control packets we have got in order to determine the data size
@@ -55,39 +64,37 @@ def read_archive(archive_filename: str) -> np.complex64:
     # Re-open the archive
     archive = Archive(archive_filename)
     metadata = archive.Metadata()
-    print('Metadata: ',metadata)
+    print('Metadata: ', metadata)
 
-    xres = metadata["acquiredXRes"]
-    yres = metadata["acquiredYRes"]
-    num_control = metadata["controlCount"]
-    num_channels = metadata["numChannels"]
+    xres = metadata['acquiredXRes']
+    num_control = metadata['controlCount']
+    num_channels = metadata['numChannels']
 
-    # initialise array to store acquired data
+    # initialize array to store acquired data
     rawdata = np.zeros((xres, num_packets, num_channels), dtype=np.complex64)
 
     # Loop over all the control packets
     for view in range(num_control):
-
         # Retrieve the next control packet
         control = archive.NextControl()
         # this is a raw control packet, but still get the next frame so that the control and frames are in sync
-        if control["opcode"] == 16:
-            next_frame = np.squeeze(archive.NextFrame())
+        if control['opcode'] == 16:
+            _ = np.squeeze(archive.NextFrame())
 
         # this is a programmable control packet, so use next frame to fill a line of rawdata
-        elif control["opcode"] == 1:
+        elif control['opcode'] == 1:
             # Assume operation == 0
-            if control["operation"] == 0:       # view = frame
+            if control['operation'] == 0:  # view = frame
                 rawdata[:, view, :] = archive.NextFrame()
             else:
                 raise ValueError('Unknown control operation')
 
     return rawdata
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     # parse arguments
-    parser = argparse.ArgumentParser(
-         description='Extract raw data from a scan archive file and export as numpy array.')
+    parser = argparse.ArgumentParser(description='Extract raw data from a scan archive file and export as numpy array.')
 
     parser.add_argument('scan_arc', type=Path, help='Path to Scan Archive')
     args = parser.parse_args()
@@ -98,5 +105,4 @@ if __name__=='__main__':
     rawdata = read_archive(str(scan_arc))
     print(f'Saving k-space with dimensions: {rawdata.shape}')
 
-    np.save(scan_arc.parent / f'{scan_arc.stem}_raw',rawdata)
-
+    np.save(scan_arc.parent / f'{scan_arc.stem}_raw', rawdata)
